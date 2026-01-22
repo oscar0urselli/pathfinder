0c464529fa78eb397e33580965b68df13c846365
Oscar Urselli <urselli.oscar@gmail.com>
2026-01-22 19:30:19 +0100

    feat: [main] Add ability to choose columns to show in the database explorer. Closes #16

--- pathfinder/src/routes/database/+page.svelte
+++ pathfinder/src/routes/database/+page.svelte
@@ -3,7 +3,17 @@
     import { invoke } from "@tauri-apps/api/core";
     import { onMount } from "svelte";
     import type { ReportType } from "$lib/schema";
+    
+    
+    function changeSelectedColumns(event: any) {
+        let formData = new FormData(selectedColumnsForm);
         
+        selectedColumns = [];
+        for (const i of formData.keys()) {
+            selectedColumns.push(i);
+        }
+    }
+    
     
     let selectedRecord: any | undefined = $state(undefined);
     
@@ -16,7 +26,12 @@
             type: string
         }[]
     } = $state({});
+    let selectedColumns = $derived(tables[selectedTable].map((v) => v.name));
+    
     let reports: ReportType[] = $state([]);
+    
+    let selectedColumnsForm: HTMLFormElement;
+    
     onMount(async () => {
         tables = await invoke("get_tables_list");
         console.log(tables[selectedTable].map((v) => v.name).includes("report"));
@@ -27,6 +42,19 @@
 <div class="container p-2">
     <div class="vstack gap-2">
         <div class="hstack gap-2">
+            <div class="dropdown">
+                <button class="btn btn-light border dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Selected columns</button>
+                <div class="dropdown-menu p-2">
+                    <form bind:this={selectedColumnsForm}>
+                    {#each tables[selectedTable] as c}
+                        <div class="form-check">
+                            <input onchange={changeSelectedColumns} name={c.name} class="form-check-input" type="checkbox" value="" id="checkDefault" checked={selectedColumns.includes(c.name)}>
+                            <label class="form-check-label" for="checkDefault">{c.name}</label>
+                        </div>
+                    {/each}
+                    </form>
+                </div>
+            </div>
             <select bind:value={selectedTable} class="form-select" aria-label="Default select example">
                 {#each Object.keys(tables) as t}
                     <option value={t} selected={t === "reports"}>{t}</option>
@@ -51,7 +79,9 @@
                 <thead>
                     <tr>
                         {#each tables[selectedTable] as c}
-                            <th scope="col">{c.name}</th>
+                            {#if selectedColumns.includes(c.name)}
+                                <th scope="col">{c.name}</th>
+                            {/if}
                         {/each}
                         <th></th>
                     </tr>
@@ -61,7 +91,9 @@
                         {#if r["report"] === selectedReport || selectedReport === "" || selectedTable === "reports"}
                             <tr>
                                 {#each tables[selectedTable] as c}
-                                    <td>{String(r[c.name]).slice(0, 36)}{String(r[c.name]).length > 36 ? "..." : ""}</td>
+                                    {#if selectedColumns.includes(c.name)}
+                                        <td>{String(r[c.name]).slice(0, 36)}{String(r[c.name]).length > 36 ? "..." : ""}</td>
+                                    {/if}
                                 {/each}
                                 <td><button onclick={() => { selectedRecord = r; }} class="btn btn-sm" data-bs-toggle="modal" data-bs-target="#record-details" aria-label="Details"><i class="bi bi-chevron-right"></i></button></td>
                             </tr>
@@ -85,7 +117,7 @@
             <div class="modal-body">
                 {#if selectedRecord}
                     {#each Object.entries(selectedRecord) as [name, r]}
-                        <p>{name}: {r}</p>
+                        <p><strong>{name}</strong>: {r}</p>
                     {/each}
                 {/if}
             </div>
